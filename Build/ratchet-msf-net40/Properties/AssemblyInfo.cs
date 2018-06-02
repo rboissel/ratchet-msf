1710434d973448964555bcd1fbc3660e4b9e1ba1
rboissel <raphael.boissel@orange.fr>
2018-06-02 15:47:51 -0700

    FIx assembly info to no longer have the .net version in the name

--- Build/ratchet-msf-net40/Properties/AssemblyInfo.cs
+++ Build/ratchet-msf-net40/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("ratchet-msf-net40")]
+[assembly: AssemblyTitle("ratchet-msf")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ratchet-msf-net40")]
+[assembly: AssemblyProduct("ratchet-msf")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
